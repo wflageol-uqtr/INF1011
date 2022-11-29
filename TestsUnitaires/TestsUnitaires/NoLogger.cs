d23407d1c1c461322369790da95dc3b311c9bc37
William Flageol <wflageol@gmail.com>
2022-11-29 10:24:34 -0500

    Ajout code principal.

---
+++ TestsUnitaires/TestsUnitaires/NoLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires
+{
+    class NoLogger : ILogger
+    {
+        public void WriteLine(string line)
+        {
+            // NOP
+        }
+    }
+}

3773966f23af1b04c28eaadd5ab93934e858ca87
William Flageol <zhevilh@users.noreply.github.com>
2025-11-11 11:08:41 -0500

    Ajout PokerWeb

---
+++ PokerWeb/PokerWeb/Hubs/PokerHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PokerWeb.Hubs
+{
+    public class PokerHub : Hub
+    {
+        public const string Url = "/pokerhub";
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connecté.");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? e)
+        {
+            Console.WriteLine($"Déconnection: {e?.Message} {Context.ConnectionId}");
+            return base.OnDisconnectedAsync(e);
+        }
+    }
+}
---
+++ PokerWeb/PokerWeb/Program.cs
@@ -0,0 +1,32 @@
+using PokerWeb.Components;
+using PokerWeb.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<PokerHub>(PokerHub.Url);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
